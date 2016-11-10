4e8439edaaecb234dcc7ab872d785a8e5500bcc2
nightcrawler086 <brian.hall4@gmail.com>
2016-11-10 14:45:20 -0500

    base script complete

--- aws/create-user_sbs.py
+++ aws/create-user_sbs.py
@@ -7,31 +7,21 @@ import argparse
 __author__ = 'nightcrawler'
 
 # Define our IAM client to talk to the API
-client = boto3.client('iam')
+iam = boto3.client('iam')
 
 # Define arguments
-# Add argument for username list
 parser = argparse.ArgumentParser(description='Create AWS Users from input file')
 parser.add_argument('-i','--input-file',help='input file',required=True)
-# Check if group is empty
-parser.add_argument('-g','--group',help='group to add users to',required=False)
-# Check if empty, then parse list of usernames
-parser.add_argument('-u','--users',help='list of usernames',required=False)
 args = parser.parse_args()
 
 # Get file extension to detect input file type
 input_file, input_file_ext = os.path.splitext(args.input_file)
 
 if input_file_ext == '.csv':
-    print("Input file is CSV")
     csv = pd.read_csv(args.input_file)
 else:
     print("Expected a CSV file, exiting...")
     quit()
-
-# This is how to print columns from the same row
-#for index, row in csv.iterrows():
-#    print row['FirstName'], row['LastName']
 
 # Empty list to fill with usernames
 new_users = []
@@ -46,37 +36,44 @@ for index, row in csv.iterrows():
     user_name = first_init.lower() + last_name.lower()
     new_users.append(user_name)
 
-# Get current users, so we don't create duplicates
+# Empty list to store current users
 current_users = []
 
-eusers = client.list_users()
+# Get current users, so we don't try to create duplicates
+eusers = iam.list_users()
 for euser in eusers.get('Users', []):
     euname = euser['UserName']
     current_users.append(euname)
 
-# Empty list to store responses
-responses = {}
+# Empty list to store responses from user creation
+responses = []
 
 # Loop through the usernames we've created and create the 
 # user account in AWS and add the users to the proper groups
-print("Creating users...")
 for user in new_users:
     # Check if user exists 
     if user in current_users:
         print("%r already exists") % user
     else:
         # If not, create the user
-        response = client.create_user(
+        response = iam.create_user(
             UserName=user
         )
         # Save response
         responses.append(response)
         # Add user to group
-        grp_rspnse = client.add_user_to_group(
+        grp_rspnse = iam.add_user_to_group(
             GroupName='NoBilling',
             UserName=user
         )
+        # Save response
+        grp_responses.append(grp_rspnse)
 
-# all the code above works.
-# want to add validation step (response code 200?)
-# and add argument to take list of users to create
+for response in responses:
+    if (response.get('ResponseMetadata'))['HTTPStatusCode'] == 200:
+        uname = (response.get('User')['UserName'])
+        print("Create user %r - OK") % uname
+    else:
+        code = (response.get('ResponseMetadata')['HTTPStatusCode'])
+        uname = (response.get('User')['UserName'])
+        print("Create user %r failed with status code %r") % (uname,code)
