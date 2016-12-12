af13f03b5c511a6693902e3405e0d8c0c696a6fb
nightcrawler086 <brian.hall4@gmail.com>
2016-12-12 11:47:23 -0500

    v1

---
+++ misc/Create-EmcopyScripts.ps1
@@ -0,0 +1,25 @@
+[CmdletBinding()]
+Param (
+	[Parameter(Mandatory=$True,Position=1)]
+	 [string]$File
+)
+
+BEGIN {
+
+	$INFILE = Import-Csv $File
+
+}
+
+PROCESS {
+
+	ForEach ($OBJ in $INFILE) {
+		$VOLNUM = $($OBJ.TargetVolume)
+		$VOLNUM = $VOLNUM.Substring($VOLNUM.get_length()-4)
+		$TGTQTREE = "ctiqt$VOLNUM"
+		Write-Output "emcopy64.exe \\$($OBJ.SourceVfiler)\$($OBJ.SourceVolume) \\$($OBJ.TargetVdm\$TGTQTREE /s /sdd /d /o /a /secfix /i /lg /purge /r:0 /w:0 /c /log:D:\emcopy-log\$($OBJ.TargetVolume).txt" | Tee-Object -FilePath .\incremental-commands.txt -Append
+		Write-Output "emcopy64.exe \\$($OBJ.SourceVfiler)\$($OBJ.SourceVolume) \\$($OBJ.TargetVdm\$TGTQTREE /s /sdd /d /o /a /i /lg /purge /r:0 /w:0 /c /log:D:\emcopy-log\$($OBJ.TargetVolume)-final.txt" | Tee-Object -FilePath .\final-commands.txt -Append
+	}
+}
+
+END{
+}
