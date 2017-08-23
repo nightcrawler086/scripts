df041d850b7d59f62373867419504d3f8ce04690
nightcrawler <brian.hall4@gmail.com>
2017-08-23 17:00:35 -0400

    phase 2

--- misc/nasccp-processor.ps1
+++ misc/nasccp-processor.ps1
@@ -1,79 +1,66 @@
 [CmdletBinding()]
 Param (
-	[Parameter(Mandatory=$False,Position=1)]
-	 [string]$InputFile
+	[Parameter(Mandatory=$True,Position=1)]
+	 [string]$MappingFile,
+
+	[Parameter(Mandatory=$True,Position=2)]
+	 [string]$NasccpFile
 )
 
 BEGIN {
 
+	# Import the NASCCP CSV File
+	$NASCCP = Import-Csv $NasccpFile
+	# Defining the Properties that we need to compare
+	$SAPROPS = @("SA_SNOW_Queue","App_SNOW_Queue","Dispatch_Alert_SNOW_Queue")
+	# Our output array
     $OUTPUT = @()
-
 }
 
 PROCESS {
 
 	$TIMER = [System.Diagnostics.StopWatch]::StartNew()
 	# Importing required files
-	$NASCCP = Import-Csv 'NASCCP.csv'
 
-	$TRACKER = Import-Csv $InputFile | Where-Object {$_.Status -ne "5. Completed" -and $_.Status -ne "4. Pending Offline"}
+	$TRACKER = Import-Csv $MappingFile | Where-Object {$_.Status -eq "5. Completed"}
 	$TRACKER = $TRACKER | Where-Object {$_.'Target VDM' -ne "" -and $_.'Target VDM' -ne "N/A" -and $_.'Target VDM' -ne $NULL}
 	$TRACKER = $TRACKER | Where-Object {$_.'Prod File System' -ne "" -and $_.'Prod File System' -ne "N/A" -and $_.'Prod File System' -ne $NULL}
+	$NASCCP = $NASCCP | Where-Object {$_."$($SAPROPS[0])" -eq "" -or $_."$($SAPROPS[0])" -eq "0" -and
+										$_."$($SAPROPS[1])" -eq "" -or $_."$($SAPROPS[1])" -eq "0" -and
+											$_."$($SAPROPS[2])" -eq "" -or $_."$($SAPROPS[2])" -eq "0"}
 	$COUNT = $TRACKER | Measure-Object
 	Write-Host "$($COUNT.Count) objects to compare"
-	#$TRACKER | Select-Object 'Target VDM','Prod File System'
 	$COUNTER = 0
 	# Conditions go here
 	ForEach ($ROW in $TRACKER) {
-<#
-		$SLICE = @()
-		If ($OBJ = $NASCCP | Where-Object {$_.NAS_VDM_vFiler -eq "$($ROW.'Target VDM')" -and $_.NAS_Volume -eq "$($ROW.'Prod File System')"}) {
-			$SLICE += $OBJ
-		} Else {
-			$VDM = "$($OBJ.NAS_VDM_vFiler)"
-			$VDM = $VDM.Replace("vnaz","vnas")
-			If ($OBJ = $NASCCP | Where-Object {$_.NAS_VDM_vFiler -eq "$VDM" -and $_.NAS_Volume -eq "$($ROW.'Prod File System')"}) {
-				$SLICE += $OBJ
+		Write-Host "$COUNTER objects compared..."
+		$VDM = "$($ROW.'Target VDM')"
+		$VDM = $VDM.Replace("vnaz","vnas")
+		$SRC = $NASCCP | Where-Object {$_.NAS_VDM_vFiler -eq "$($ROW.'NETAPP Prod VFiler')" -and $_.NAS_Volume -eq "$($ROW.'NETAPP Prod Volume')"}
+		$TGT = $NASCCP | Where-Object {$_.NAS_VDM_vFiler -eq "$VDM" -and $_.NAS_Volume -eq "$($ROW.'Prod File System')"}
+		If ($TGT -and $SRC) {
+			ForEach ($PROP in $SAPROPS) {
+				If ($($SRC."$PROP") -ne "" -and $($SRC."$PROP") -ne "0") {
+					$TGT.$PROP="$($SRC."$PROP")"
+					$TGT = $TGT | Add-Member -NotePropertyMembers @{"${PROP}Modified"="SourceVolRecord"}
+					Write-Host "Used Source Volume Record for $($TGT.NAS_VDM_vFiler):$($TGT.NAS_Volume) $PROP"
+				}
+			}
+			ElseIf ($TGT) {
+				If ($($ROW.'SA SNOW Queue Name') -ne "" -or $($ROW.'SA SNOW Queue Name') -eq "0" -or
+					$($ROW.'SA SNOW Queue Name') -eq "#N/A" -or $($ROW.'SA SNOW Queue Name') -eq "NULL") {
+					ForEach ($PROP in $SAPROPS) {
+						$TGT.$PROP="$($ROW.'SA SNOW Queue Name')"
+						$TGT = $TGT | Add-Member -NotePropertyMembers @{"${PROP}Modified"="Tracker"}
+						Write-Host "Tracker for $($TGT.NAS_VDM_vFiler):$($TGT.NAS_Volume) Property $PROP"
+					}
+				}
 			}
 		}
-
-		ForEach ($REC in $SLICE) {
-			If ($($REC.NAS_VDM_vFiler) -and $($REC.NAS_Volume)) {
-#>
-				$COUNTER++
-				$OUTPUT += New-Object -TypeName PSObject -Property @{
-					ID = $OBJ.ID;
-					NAS_Frame_PROD = $ROW.'Target Prod Frame';
-					NAS_VDM_vFiler = $ROW.'Target VDM';
-					NAS_Alias_vFiler = $ROW.'3 DNS cname entry';
-					NAS_Volume = $ROW.'Prod File System';
-					NAS_Qtree = $ROW.'Qtree & share name';
-					NAS_Frame_DR = $ROW.'Target COB VNX Frame'; 
-					Vendor = "emc";
-					Business_Group_Distribution_List = "*CTI GLOBAL EX CIS HIA NAS TR & MIGRATIONS";
-					SA_Support_Group_Distribution_List = "*CTI GLOBAL EX CIS HIA NAS TR & MIGRATIONS";
-					Apps_Support_Group_Distribution_List = "*CTI GLOBAL EX CIS HIA NAS TR & MIGRATIONS";
-					Business_Sector = "CRP";
-					SA_SNOW_Queue = "CTI GL EX CIS HIA NAS TR & MIGRATIONS";
-					App_SNOW_Queue = "CTI GL EX CIS HIA NAS TR & MIGRATIONS";
-					Dispatch_Alert_SNOW_Queue = "CTI GL EX CIS HIA NAS TR & MIGRATIONS";
-					AlertSuppress = "1";
-					SNOW_ChangeManagement = "";
-					SNOW_GreenZoneStart = "";
-					SNOW_GreenZoneEnd = "";
-					Snow_GreenZoneFrequency = "";
-					Netbackup_Policy = $ROW.'New Tape Policy Name';
-					NotificationSetting = "";
-					INC = "";
-					INC_Justification = "";
-					AppID = "160157"
-				}
-			#}
-		#}
+		$COUNTER++
+		$OUTPUT += $TGT
 	}
-	Write-Host "$COUNTER objects matched in $($TIMER.Elapsed.TotalSeconds) seconds"
 }
-
 
 END{
 
