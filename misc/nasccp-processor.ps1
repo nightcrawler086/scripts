ea11c1b5b7faaef7806d7e062112b5bb0ca34927
nightcrawler086 <brian.hall4@gmail.com>
2017-08-29 07:14:52 -0400

    works

--- misc/nasccp-processor.ps1
+++ misc/nasccp-processor.ps1
@@ -14,6 +14,7 @@ BEGIN {
 	# Defining the Properties that we need to compare
 	$SAPROPS = @("SA_SNOW_Queue","App_SNOW_Queue","Dispatch_Alert_SNOW_Queue")
 	# Base output object (essentially a row in the nasccp file)
+	<#
 	$HASH = @{ AlertSuppress = $($TGT.AlertSuppress);
 		AppID = $($TGT.AppID);
 		Apps_Support_Group_Distribution_List = $($TGT.Apps_Support_Group_Distribution_List);
@@ -42,6 +43,7 @@ BEGIN {
 		SNOW_GreenZoneStart = $($TGT.SNOW_GreenZoneStart);
 		Vendor = $($TGT.Vendor)
 	}
+	#>
 	# Our output array
     $OUTPUT = @()
 }
@@ -72,7 +74,34 @@ PROCESS {
 		#Write-Host "Matching Source Volume Record: $SRC"
 		#Write-Host "Matching Target Volume Record: $TGT"
 		If ($TGT -and $SRC) {
-			$OBJ = New-Object -TypeName PSObject -Property $HASH
+			$OBJ = New-Object -TypeName PSObject -Property @{ AlertSuppress = $($TGT.AlertSuppress);
+				AppID = $($TGT.AppID);
+				Apps_Support_Group_Distribution_List = $($TGT.Apps_Support_Group_Distribution_List);
+				Archived_By = $($TGT.Archived_By);
+				Archived_Date = $($TGT.Archived_Date);
+				Business_Group_Distribution_List = $($TGT.Business_Group_Distribution_List);
+				Business_Sector = $($TGT.Business_Sector);
+				CCP_Record_Created_By = $($TGT.CCP_Record_Created_By);
+				CCP_Record_Create_Date = $($TGT.CCP_Record_Create_Date);
+				CCP_Record_Modified_By = $($TGT.CCP_Record_Modified_by);
+				CCP_Record_Modify_Date = $($TGT.CCP_Record_Modify_Date);
+				ID = $($TGT.ID);
+				NAS_Alias_vFiler = $($TGT.NAS_Alias_vFiler);
+				NAS_Frame_DR = $($TGT.NAS_Frame_DR);
+				NAS_Frame_PROD = $($TGT.NAS_Frame_PROD);
+				NAS_Qtree = $($TGT.NAS_Qtree);
+				NAS_VDM_vFiler = $($TGT.NAS_VDM_vFiler);
+				NAS_Volume = $($TGT.NAS_Volume);
+				Netbackup_Policy = $($TGT.Netbackup_Policy);
+				NotificationSetting = $($TGT.NotificationSetting);
+				Region = $($TGT.Region);
+				SA_Support_Group_Distribution_List = $($TGT.SA_Support_Group_Distribution_List);
+				SNOW_ChangeManagement = $($TGT.SNOW_ChangeManagement);
+				SNOW_GreenZoneEnd = $($TGT.SNOW_GreenZoneEnd);
+				SNOW_GreenZoneFrequency = $($TGT.SNOW_GreenZoneFrequency);
+				SNOW_GreenZoneStart = $($TGT.SNOW_GreenZoneStart);
+				Vendor = $($TGT.Vendor)
+			}
 			#Write-Host "Found Source and Target volume match in NASCCP"
 			ForEach ($PROP in $SAPROPS) {
 				If ($($SRC.$PROP) -ne "" -and $($SRC.$PROP) -ne "0") {
@@ -83,7 +112,34 @@ PROCESS {
 			}
 		}
 		ElseIf ($TGT) {
-			$OBJ = New-Object -TypeName PSObject -Property $HASH
+			$OBJ = New-Object -TypeName PSObject -Property @{ AlertSuppress = $($TGT.AlertSuppress);
+				AppID = $($TGT.AppID);
+				Apps_Support_Group_Distribution_List = $($TGT.Apps_Support_Group_Distribution_List);
+				Archived_By = $($TGT.Archived_By);
+				Archived_Date = $($TGT.Archived_Date);
+				Business_Group_Distribution_List = $($TGT.Business_Group_Distribution_List);
+				Business_Sector = $($TGT.Business_Sector);
+				CCP_Record_Created_By = $($TGT.CCP_Record_Created_By);
+				CCP_Record_Create_Date = $($TGT.CCP_Record_Create_Date);
+				CCP_Record_Modified_By = $($TGT.CCP_Record_Modified_by);
+				CCP_Record_Modify_Date = $($TGT.CCP_Record_Modify_Date);
+				ID = $($TGT.ID);
+				NAS_Alias_vFiler = $($TGT.NAS_Alias_vFiler);
+				NAS_Frame_DR = $($TGT.NAS_Frame_DR);
+				NAS_Frame_PROD = $($TGT.NAS_Frame_PROD);
+				NAS_Qtree = $($TGT.NAS_Qtree);
+				NAS_VDM_vFiler = $($TGT.NAS_VDM_vFiler);
+				NAS_Volume = $($TGT.NAS_Volume);
+				Netbackup_Policy = $($TGT.Netbackup_Policy);
+				NotificationSetting = $($TGT.NotificationSetting);
+				Region = $($TGT.Region);
+				SA_Support_Group_Distribution_List = $($TGT.SA_Support_Group_Distribution_List);
+				SNOW_ChangeManagement = $($TGT.SNOW_ChangeManagement);
+				SNOW_GreenZoneEnd = $($TGT.SNOW_GreenZoneEnd);
+				SNOW_GreenZoneFrequency = $($TGT.SNOW_GreenZoneFrequency);
+				SNOW_GreenZoneStart = $($TGT.SNOW_GreenZoneStart);
+				Vendor = $($TGT.Vendor)
+			}
 			#Write-Host "Only found target volume in NASCCP"
 			If ($($ROW.'SA SNOW Queue Name') -ne "" -and $($ROW.'SA SNOW Queue Name') -ne "0" -and
 				$($ROW.'SA SNOW Queue Name') -ne "#N/A" -and $($ROW.'SA SNOW Queue Name') -ne "NULL") {
