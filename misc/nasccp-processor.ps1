8d0218551eac271f3cb038a51bf6d7c0524cbf62
nightcrawler086 <brian.hall4@gmail.com>
2017-10-04 12:34:32 -0400

    new version of nasccp

--- misc/nasccp-processor.ps1
+++ misc/nasccp-processor.ps1
@@ -31,9 +31,6 @@ PROCESS {
 	$COUNT = $TRFILE | Measure-Object
 	Write-Host "$($COUNT.Count) objects in tracker to compare"
 	$TOTAL = 0
-	$STCOUNT = 0
-	$TCOUNT = 0
-	$NONE = 0
 	# For each row in our tracker
 	ForEach ($R in $TRFILE) {
 		Write-Progress -Activity "Processed: $TOTAL | Complete: $($TOTAL / $($COUNT.Count) * 100)`%"
@@ -50,19 +47,32 @@ PROCESS {
 			ForEach ($P in $SAPROPS) {
 				# If the Target record's SAPROPS don't need updating
 				If ( $($T.$P) -ne ""  -and $($T.$P) -ne "0" -and $($T.$P) -notlike "N?A" -and $($T.$P) -notlike "*NAS TR & MIGRATIONS*") {
-					$OBJ | Add-Member -NotePropertyMembers @{"${P}Modified"="OriginalSourceFound"}
-				} 
+					$OBJ | Add-Member -NotePropertyMembers @{"${P}_Source"="TargetVolumeRecord"}
+					$OBJ | Add-Member -NotePropertyMembers @{"${P}_Original"="$($T.$P)"}
+				}
+				# If Source Volume Record is Valid, use it
 				ElseIf ( $($S.$P) -ne "" -and $($S.$P) -ne "0" -and $($S.$P) -notlike "N*A" -and $($T.$P) -notlike "*NAS TR & MIGRATIONS*") {
 					# Replace target property with source property
 					$OBJ.$P = "$($S.$P)"
 					# Add new property showing where the above value is sourced
-					$OBJ | Add-Member -NotePropertyMembers @{"${P}Modified"="SourceVolume"}
-				} Else {
-					$OBJ | Add-Member -NotePropertyMembers @{"${P}Modified"="SourcePropInvalid"}
+					$OBJ | Add-Member -NotePropertyMembers @{"${P}_Source"="SourceVolumeRecord"}
+					$OBJ | Add-Member -NotePropertyMembers @{"${P}_Original"="$($T.$P)"}
+				}
+				# If the Tracker's value is valid, use it
+				ElseIf ($($R.'SA SNOW Queue Name') -ne "" -and $($R.'SA SNOW Queue Name') -ne "0" -and
+				$($R.'SA SNOW Queue Name') -ne "#N/A" -and $($R.'SA SNOW Queue Name') -ne "NULL") {
+					# Replaces property in target object with value from our tracker
+					$OBJ.$P = $($R.'SA SNOW Queue Name')
+					$OBJ | Add-Member -NotePropertyMembers @{"${P}_Source"="Tracker"}
+					$OBJ | Add-Member -NotePropertyMembers @{"${P}_Original"="$($T.$P)"}
+				}
+				# Else do nothing
+				Else {
+					$OBJ | Add-Member -NotePropertyMembers @{"${P}_Source"="None"}
+					$OBJ | Add-Member -NotePropertyMembers @{"${P}_Original"="$($T.$P)"}
 				}		
 			}
 			$OUTPUT += $OBJ
-			#$STCOUNT++
 		}
 		ElseIf (!$S -and $T) {
 			# Creates an empty hash table
@@ -73,19 +83,22 @@ PROCESS {
 			# Creates a new object with our hash table
 			$OBJ = New-Object -TypeName PSObject -Property $HT
 			If ( !$($T.$P) -and $($T.$P) -ne "0" -and $($T.$P) -notlike "N?A" -and $($T.$P) -notlike "*NAS TR & MIGRATIONS*") {
-					$OBJ | Add-Member -NotePropertyMembers @{"${P}Modified"="OriginalSourceNotFound"}
-			} Else {
-				If ($($R.'SA SNOW Queue Name') -ne "" -and $($R.'SA SNOW Queue Name') -ne "0" -and
-					$($R.'SA SNOW Queue Name') -ne "#N/A" -and $($R.'SA SNOW Queue Name') -ne "NULL") {
-						ForEach ($P in $SAPROPS) {
-							# Replaces property in target object with value from our tracker
-							$OBJ.$P = $($R.'SA SNOW Queue Name')
-							$OBJ | Add-Member -NotePropertyMembers @{"${P}Modified"="Tracker"}
-						}
-					$OUTPUT += $OBJ
-					#$TCOUNT++
+					$OBJ | Add-Member -NotePropertyMembers @{"${P}_Source"="TargetVolumeRecord"}
+					$OBJ | Add-Member -NotePropertyMembers @{"${P}_Original"="$($T.$P)"}
+			} ElseIf ($($R.'SA SNOW Queue Name') -ne "" -and $($R.'SA SNOW Queue Name') -ne "0" -and
+				$($R.'SA SNOW Queue Name') -ne "#N/A" -and $($R.'SA SNOW Queue Name') -ne "NULL") {
+				ForEach ($P in $SAPROPS) {
+					# Replaces property in target object with value from our tracker
+					$OBJ.$P = $($R.'SA SNOW Queue Name')
+					$OBJ | Add-Member -NotePropertyMembers @{"${P}_Source"="Tracker"}
+					$OBJ | Add-Member -NotePropertyMembers @{"${P}_Original"="$($T.$P)"}
 				}
 			}
+			Else {
+				$OBJ | Add-Member -NotePropertyMembers @{"${P}_Source"="None"}
+				$OBJ | Add-Member -NotePropertyMembers @{"${P}_Original"="$($T.$P)"}
+			}
+			$OUTPUT += $OBJ
 		}
 		ElseIf ($S -and !$T) {
 			$OBJ = New-Object -TypeName PSObject -Property @{ AlertSuppress = "0";
@@ -124,19 +137,27 @@ PROCESS {
 					# Replace target property with source property
 					$OBJ.$P = "$($S.$P)"
 					# Add new property showing where the above value is sourced
-					$OBJ | Add-Member -NotePropertyMembers @{"${P}Modified"="SourcePropNoTarget"}
-				} Else {
+					$OBJ | Add-Member -NotePropertyMembers @{"${P}_Source"="SourceVolumeRecord"}
+					$OBJ | Add-Member -NotePropertyMembers @{"${P}_Original"="None"}
+				}
+				ElseIf ($($R.'SA SNOW Queue Name') -ne "" -and $($R.'SA SNOW Queue Name') -ne "0" -and
+				$($R.'SA SNOW Queue Name') -ne "#N/A" -and $($R.'SA SNOW Queue Name') -ne "NULL") {
 					$OBJ.$P = "$($R.'SA SNOW Queue Name')"
-					$OBJ | Add-Member -NotePropertyMembers @{"${P}Modified"="TrackerNoTarget"}
-				}		
+					$OBJ | Add-Member -NotePropertyMembers @{"${P}_Source"="Tracker"}
+					$OBJ | Add-Member -NotePropertyMembers @{"${P}_Original"="None"}
+				}
+				Else {
+				$OBJ | Add-Member -NotePropertyMembers @{"${P}_Source"="None"}
+				$OBJ | Add-Member -NotePropertyMembers @{"${P}_Original"="$($T.$P)"}
+				}	
 			}
 			$OUTPUT += $OBJ
 		}
-		Else { $OUTPUT += New-Object -TypeName PSObject -Property @{ AlertSuppress = "0";
+		Else { 
+			$OBJ = New-Object -TypeName PSObject -Property @{ AlertSuppress = "0";
 				AppID = "";
 				Apps_Support_Group_Distribution_List = "";
-				App_SNOW_Queue = $($R.'SA SNOW Queue Name');
-				App_SNOW_QueueModified = "NewTracker";
+				App_SNOW_Queue = "";
 				Archived_By = "";
 				Archived_Date = "";
 				Business_Group_Distribution_List = "";
@@ -145,8 +166,7 @@ PROCESS {
 				CCP_Record_Create_Date = "";
 				CCP_Record_Modified_By = "";
 				CCP_Record_Modify_Date = "";
-				Dispatch_Alert_SNOW_Queue = $($R.'SA SNOW Queue Name');
-				Dispatch_Alert_SNOW_QueueModified = "NewTracker";
+				Dispatch_Alert_SNOW_Queue = "";
 				ID = "";
 				NAS_Alias_vFiler = $($R.'3 DNS cname entry');
 				NAS_Frame_DR = $($R.'Target Cob VNX Frame');
@@ -157,16 +177,29 @@ PROCESS {
 				Netbackup_Policy = $($R.'New Tape Policy Name');
 				NotificationSetting = "";
 				Region = "NAM";
-				SA_SNOW_Queue = $($R.'SA SNOW Queue Name');
-				SA_SNOW_QueueModified = "NewTracker";
+				SA_SNOW_Queue = "";
 				SA_Support_Group_Distribution_List = $($R.'SA SNOW Queue Name');
 				SNOW_ChangeManagement = "";
 				SNOW_GreenZoneEnd = "";
 				SNOW_GreenZoneFrequency = "";
 				SNOW_GreenZoneStart = "";
 				Vendor = "emc"
-			} 
-			#$NONE++ 
+			}
+			ForEach ($P in $SAPROPS) {
+				If ($($R.'SA SNOW Queue Name') -ne "" -and $($R.'SA SNOW Queue Name') -ne "0" -and
+				$($R.'SA SNOW Queue Name') -ne "#N/A" -and $($R.'SA SNOW Queue Name') -ne "NULL") {
+					# Replaces property in target object with value from our tracker
+					$OBJ.$P = $($R.'SA SNOW Queue Name')
+					$OBJ | Add-Member -NotePropertyMembers @{"${P}_Source"="Tracker"}
+					$OBJ | Add-Member -NotePropertyMembers @{"${P}_Original"="None"}
+				} 
+				Else {
+					$OBJ.$P = "CTI GL EX CIS HIA NAS TR & MIGRATIONS"
+					$OBJ | Add-Member -NotePropertyMembers @{"${P}_Source"="Default"}
+					$OBJ | Add-Member -NotePropertyMembers @{"${P}_Original"="None"}
+				}
+			}
+			$OUTPUT += $OBJ
 		}
 		$TOTAL++
 	}
