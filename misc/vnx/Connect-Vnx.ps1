1fe3467a33391adb9f6daf12b8b7784943103c3d
nightcrawler086 <brian.hall4@gmail.com>
2018-12-09 11:43:03 -0500

    cleaning up

--- misc/vnx/Connect-Vnx.ps1
+++ misc/vnx/Connect-Vnx.ps1
@@ -19,64 +19,66 @@ function Connect-Vnx
          ValueFromPipelineByPropertyName=$true,
          Position=0)]
          [ValidateNotNullOrEmpty()]
-         [string]$VNX,
+         [string]$Name,
         # Specify the credential object
-        [Parameter(Mandatory=$true,
+        # If none specified, we can promptre
+        [Parameter(Mandatory=$false,
          ValueFromPipelineByPropertyName=$false,
          Position=1)]
          [ValidateNotNullOrEmpty()]
-         [System.Management.Automation.PSCredential]$Credential,
-        # Using this switch will not verify the SSL Certificate
-        [Parameter(Mandatory=$false,
-         ValueFromPipelineByPropertyName=$false)]
-         [switch]$Insecure
+         [System.Management.Automation.PSCredential]$Credential
     )
     BEGIN {
-        If ($Insecure) {
-            # This disables certificate checking, so the self-signed certs dont' stop us
-            [system.net.servicepointmanager]::Servercertificatevalidationcallback = {$true}
-        }
-        Else {
-            Write-Host -ForeGroundColor Yellow "This machine must trust the SSL Certificate of the VNX for the connection to suceed"
+        # This disables certificate checking, so the self-signed certs dont' stop us
+        Write-Host -Foreground Yellow "Accepting control station certificate without validating..."
+        [system.net.servicepointmanager]::Servercertificatevalidationcallback = {$true}
+        If (!$Credential) {
+            $Credential = Get-Credential -Title "${Name}" -Message "Enter credentials for ${Name}"
         }
         # Below two lines are how we retrieve the plain text version
         # of username and password
-        $USER = $Credential.GetNetworkCredential().UserName
-        $PASS = $Credential.GetNetworkCredential().Password
-        $LOGINURI = "https://${VNX}/Login"
+        $user = $Credential.GetNetworkCredential().UserName
+        $pass = $Credential.GetNetworkCredential().Password
+        $loginuri = "https://${Name}/Login"
         # Credentials provided in the body
         # They will be sent via an HTTPS connection so
         # encrypted in flight
-        $BODY = "user=${USER}&password=${PASS}&Login=Login"
+        $body = "user=${USER}&password=${PASS}&Login=Login"
         # Content-Type header
-        $HEADERS = @{"Content-Type" = "x-www-form-urlencoded"}
+        $headers = @{"Content-Type" = "x-www-form-urlencoded"}
         # URL to hit for queries
-        $APIURI = "https://${VNX}/servlets/CelerraManagementServices"
+        $apiuri = "https://${Name}/servlets/CelerraManagementServices"
         # Standard "top" of XML Sheet
-        $XMLTOP = '<?xml version="1.0" encoding="UTF-8" standalone="yes"?>'
+        $xmltop = '<?xml version="1.0" encoding="UTF-8" standalone="yes"?>'
         # Standard format of XML Shee
         # Can specify the API version here, but letting the system default to
         # its version so this will work on Celerra (hopefully) and VNX
-        $XMLFORMAT = '<RequestPacket xmlns="http://www.emc.com/schemas/celerra/xml_api" >'
+        $xmlformat = '<RequestPacket xmlns="http://www.emc.com/schemas/celerra/xml_api" >'
         # Standard beginning of a query
-        $QRYBEGIN = '<Request><Query>'
+        $qrybegin = '<Request><Query>'
         # Line specifying the parameters we're querying
-        $QRY = "<CelerraSystemQueryParams/>"
-        $QRYEND= '</Query></Request>'
+        $qry = "<CelerraSystemQueryParams/>"
+        $qryend= '</Query></Request>'
         # Standard Footer for XML Sheet
-        $XMLFOOTER = '</RequestPacket>'
+        $xmlfooter = '</RequestPacket>'
         # Adding all the pieces together
-        $BODY = $XMLTOP + $XMLFORMAT + $QRYBEGIN + $QRY + $QRYEND + $XMLFOOTER
+        $request = $xmltop + $xmlformat + $qrybegin + $qry + $qryend + $xmlfooter
     }
     PROCESS {
-        $LOGIN = Invoke-RestMethod -Uri $LOGINURI -Method 'POST' -Body $BODY -SessionVariable WS
-        If ($LOGIN.StatusCode -eq 200) {
-            $RESPONSE = Invoke-RestMethod -Uri $APIURI -WebSession $WS -Headers $HEADERS -Body $BODY -Method Post
+        try {
+            $login = Invoke-WebRequest -Uri $loginuri -Method Post -Body $body -SessionVariable ws
+        }
+        catch {
+            Write-Host -ForegroundColor Red "Something went wrong...check your credentials"
+        }
+        If ($login.StatusCode -eq 200) {
+            Set-Variable -Name CurrentVnxFrame -Value $ws -Scope Global
+            $response = Invoke-RestMethod -Uri $apiuri -WebSession $CurrentVnxFrame -Headers $headers -Body $request -Method Post
+
         }
     }
     END {
-        If ($LOGIN) {
-            $global:CurrentVnxSystem = $WS
-        }
+        
     }
 }
+Connect-Vnx
